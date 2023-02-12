a18909c8ee8de39bcd2786c702132b839bc8f9f5
Mathieu VALERY <66632538+mathieu-valery@users.noreply.github.com>
2023-02-12 20:51:54 +0100

    back: new endpoint filter_measurement

--- backend/app/controllers/api/v1/measurements_controller.rb
+++ backend/app/controllers/api/v1/measurements_controller.rb
@@ -3,4 +3,22 @@ class Api::V1::MeasurementsController < ApplicationController
         @measurements = Measurement.all
         render json: @measurements
     end
+
+    def filter_measurements
+        room_names = filter_params[:room_names]
+        measure_types = filter_params[:measure_types]
+        @measurements = Measurement.where(room_name: room_names, measure_type: measure_types)
+        render json: @measurements
+    end
+
+    private
+
+    def filter_params
+        params.permit(
+            :room_names => [],
+            :measure_types => []
+        )
+      end
+
+
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :measurements, only: [:index]
+      post 'filter_measurements', action: :filter_measurements, controller: :measurements
     end
   end
 end
