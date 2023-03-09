e1793b07b10ba4150e637119603439e015d2b116
Mathieu VALERY <66632538+mathieu-valery@users.noreply.github.com>
2023-03-09 12:37:33 +0100

    puts room name in payload

--- backend/app/controllers/api/v1/measurements_controller.rb
+++ backend/app/controllers/api/v1/measurements_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::MeasurementsController < ApplicationController
     def index
         if filter_params[:room_names].present? && filter_params[:measure_types].present?
-            @measurements = Measurement.joins(:room).
+            @measurements = Measurement.eager_load(:room).
             where(room: {name: filter_params[:room_names]}).
             where(measure_type: filter_params[:measure_types])
 
         elsif filter_params[:room_names].present?
-            @measurements = Measurement.joins(:room).
+            @measurements = Measurement.eager_load(:room).
             where(room: {name: filter_params[:room_names]})
 
         elsif filter_params[:measure_types].present?
@@ -16,6 +16,7 @@ class Api::V1::MeasurementsController < ApplicationController
         else
             @measurements = Measurement.all
         end
+        puts @measurements.count
         render json: @measurements
     end
 
--- backend/app/models/measurement.rb
+++ backend/app/models/measurement.rb
@@ -1,4 +1,15 @@
 class Measurement < ApplicationRecord
     belongs_to :instrument
     belongs_to :room
+
+    def room_name
+        self.room.name
+    end
+
+    def as_json(options={})
+        super(
+            only: [:id, :timestamp, :measure_type, :measure_float, :instrument_id],
+            methods: [:room_name]
+        )
+    end
 end
